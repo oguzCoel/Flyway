f05b78b8ae2a6d1a51a9cddbb90909336d68a4f5
oguzCoel <oguzcoel@gmail.com>
2014-12-08 23:25:09 +0100

    Version 1.0

---
+++ src/main/java/ch/hfict/service/impl/PasswordTokenManager.java
@@ -0,0 +1,19 @@
+package ch.hfict.service.impl;
+
+import ch.hfict.model.User;
+
+
+public interface PasswordTokenManager {
+
+    /**
+     * {@inheritDoc}
+     */
+    String generateRecoveryToken(User user);
+
+    /**
+     * {@inheritDoc}
+     */
+    boolean isRecoveryTokenValid(User user, String token);
+
+    void invalidateRecoveryToken(User user, String token);
+}
